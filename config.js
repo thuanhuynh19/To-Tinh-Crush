e3d2b807abb7bccfb6dd03950b3fd36ba843a1a3
thuanhuynh19 <88098383+thuanhuynh19@users.noreply.github.com>
2021-07-28 22:04:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ Lạc vào tim anh lẻ loi
     btnYes: 'Vẫn cứ là iu eimmm <33',
     btnNo: 'Khum, em gái à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu eim <3',
-    btnReply: 'Gửi cho eim <3',
+    btnReply: 'Gửi cho eimmm <3',
     reply: 'iu thì iu mà hong iu thì iu <33333333',
-    mess: 'Eim biết màààà 🥰. Yêu anh nhèo nhèoo 😘😘',
+    mess: 'Eim biết màààà 🥰. iu anh nhèo nhèoo 😘😘',
     messDesc: 'chúc anh iu có mụt sinh nhật thật dzui dzẻ và sẽ iu eim nhèo hưnnnnn moa moa 😘😘😘 cảm ưn anh đã luôn ở cạnh eim lúc mà eim pùnn gòi còn chọc em cười nữa :3',
     btnAccept: 'Eo eim nhèooo <3',
     messLink: 'https://bitly.com.vn/7qpivx' //link mess của các bạn. VD: https://m.me/nam.nodemy
