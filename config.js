2aba3e36d8e7b8928d89afd35362e849c1780e57
thuanhuynh19 <88098383+thuanhuynh19@users.noreply.github.com>
2021-07-28 21:38:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Aiu àààààààààààààà <3',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vẫn cứ là iu eimmm <33',
+    btnNo: 'Khum, em gái à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'chúc anh iu có mụt sinh nhật thật dzui dzẻ và sẽ iu eim nhèo hưnnnnn moa moa 😘😘😘 cảm ưn anh đã luôn ở cạnh eim lúc mà eim pùnn gòi còn chọc em cười nữa :3',
+    btnAccept: 'Eo eim nhèooo <3',
+    messLink: 'https://bitly.com.vn/7qpivx' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
